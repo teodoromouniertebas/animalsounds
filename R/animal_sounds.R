62b5bd090b2b57ec852d94c7aa31559378932ba8
teodoromouniertebas <tmt.m.c@hotmail.fr>
2025-06-16 12:28:44 +0200

    use cli and rlang

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -11,8 +11,23 @@
 #'
 #' @examples
 #' animal_sounds("dog", "woof")
+
 animal_sounds <- function(animal, sound) {
-    stopifnot(is.character(animal) & length(animal) == 1)
-    stopifnot(is.character(sound) & length(sound) == 1)
+
+    if (!rlang::is_character(animal, n = 1)) {
+      cli::cli_abort(
+       c("{.var animal} must be a single string!",
+         "i" = "It was {.type {animal}} of length {length(animal)} instead.")
+      )
+
+    }
+
+    if (!rlang::is_character(sound, n = 1)) {
+  cli::cli_abort(
+       c("{.var sound} must be a single string!",
+         "i" = "It was {.type {sound}} of length {length(sound)} instead.")
+      )
+
+    }
     paste0("The ", animal, " goes ", sound, "!")
 }
