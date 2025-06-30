201a10e860bf6bcf011d645fe34b726221daf042
teodoromouniertebas <tmt.m.c@hotmail.fr>
2025-06-30 12:10:30 +0200

    add testthat to suggests

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -17,7 +17,8 @@ animal_sounds <- function(animal, sound) {
     if (!rlang::is_character(animal, n = 1)) {
       cli::cli_abort(
        c("{.var animal} must be a single string!",
-         "i" = "It was {.type {animal}} of length {length(animal)} instead.")
+         "i" = "It was {.type {animal}} of length {length(animal)} instead."),
+    class = "error_not_single_string"
       )
 
     }
@@ -25,7 +26,8 @@ animal_sounds <- function(animal, sound) {
     if (!rlang::is_character(sound, n = 1)) {
   cli::cli_abort(
        c("{.var sound} must be a single string!",
-         "i" = "It was {.type {sound}} of length {length(sound)} instead.")
+         "i" = "It was {.type {sound}} of length {length(sound)} instead."),
+    class = "error_not_single_string"
       )
 
     }
---
+++ tests/testthat/test-animal_sounds.R
@@ -0,0 +1,22 @@
+test_that("animal_sounds produces expected strings", {
+
+  dog_woof <- animal_sounds("dog", "woof")
+  expect_equal(dog_woof, "The dog goes woof!")
+
+  expect_equal(animal_sounds("cat", "miaow"), "The cat goes miaow!")
+
+})
+
+
+test_that("handles invalid inputs", {
+
+    expect_error(animal_sounds("dog", c("woof", "bow wow wow")),
+                 class = "error_not_single_string")
+
+    expect_error(animal_sounds("dog", c("woof", "bow wow wow")),
+                 class = "error_not_single_string")
+
+    expect_error(animal_sounds(factor("cat"), "miaow"),
+                 class = "error_not_single_string")
+
+})
