6205af169e48578c2fc1732597073ae03369141e
teodoromouniertebas <tmt.m.c@hotmail.fr>
2025-06-30 12:36:50 +0200

    add new test 2

--- tests/testthat/test-animal_sounds.R
+++ tests/testthat/test-animal_sounds.R
@@ -19,4 +19,12 @@ test_that("handles invalid inputs", {
     expect_error(animal_sounds(factor("cat"), "miaow"),
                  class = "error_not_single_string")
 
+
+})
+
+test_that("error message for invalid input", {
+
+  expect_snapshot(animal_sounds("dog", c("woof", "bow wow wow")),
+                  error = TRUE)
+
 })
